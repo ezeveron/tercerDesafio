6332b1e4025ce340c235b1936847a61fade25b81
ezeveron <eze_veron@hotmail.com>
2024-04-10 22:49:59 -0300

    integradopr

--- src/app.js
+++ src/app.js
@@ -2,54 +2,37 @@
 
 import express from "express";
 import handlebars from "express-handlebars";
-import{ Server} from "socket.io";
-
+import productRouter from "./routes/product.router.js";
+import cartRouter from "./routes/cartRoutes.js";
+import viewsRouter from "./routes/viewsRoutes.js";
 import __dirname from "./utils.js";
-import { ProductManager } from "./ProductManager.js";
+import{ Server} from "socket.io";
+import websocket from "webSocket.js" ;
 
-const app = express();
 
-app.use(express.urlencoded({extended: true}));
+const uri = "mongodb+srv://eze33veron:<25defebrero>@cluster0.ugmh6sf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0mongodb+srv://eze33veron:<password>@cluster0.ugmh6sf.mongodb.net/";
+mongoose.connect(uri);
+
+
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname + '/../views');
+app.set('view engine', 'handlebars');
+
+
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
 
-app.engine("handlebars", handlebars.engine());
-app.set("views",`${__dirname}/views`);
-app.set("view engine", "handlebars");
 
-const UM = new ProductManager();
-
-app.get("/api/products", (req, res) => {
-    res.send(UM.GetAllProduct());
-});
-
-app.post("/api/products", (req, res) => {
-    const response = UM.CreateProduct(req.body);
-    res.status(201).send(response);
-});
-
-//View endpoint
-app.get("/", (req, res) => {
-    res.render(
-        "index",
-        {
-            title: "Coderhouse"
-        }
-    )
-});
+app.use('/api/products', productRouter);
+app.use('/api/carts', cartRouter);
+app.use('/products', viewsRouter);
 
 const PORT = 8080;
-const httpServer=app.listen(PORT, () => {
-    console.log(`Servidor activo en http://localhost:${PORT}`);
+const httpServer = app.listen(PORT, () => {
+    console.log(`Start server in PORT ${PORT}`);
 });
 
-const socketServer = new Server (httpServer)
+const io = new Server(httpServer);
 
-socketServer.on("connection", socket=>{
-    console.log("Nuevo producto creado", socket.id)
-
-
-    socket.on("messege",data=>{
-        console.log(data)
-        socketServer.emit("messegeShow",data)
-    })
-})
+websocket(io);
